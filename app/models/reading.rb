6b6f628958528301b0da4045ef9de9c68fb12b96
Sean Rabaut <rabautse@gmail.com>
2014-09-06 05:44:51 -0400

    Reading model validations

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,6 +1,17 @@
 class Reading < ActiveRecord::Base
   belongs_to :driver
 
+  validates :value,
+    presence: true
+
+  validates :name,
+    presence: true,
+    inclusion: values
+
+  validates :timestamp,
+    presence: true
+
+
   def self.values 
     [
       'EngRPM',
