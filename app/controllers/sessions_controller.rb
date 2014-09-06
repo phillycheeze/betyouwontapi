b2aa2cdb0938f2ad5b96a71f0b94aa92ac78c5e1
Sean Rabaut <rabautse@gmail.com>
2014-09-06 03:31:12 -0400

    Typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   before_action :authenticate, only: :destroy
 
   def create
-    user = User.find_by_email(params[:session][:email].downcase)
+    user = Manager.find_by_email(params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
       render json: { auth_token: user.auth_token }
