f4b01735c97f9ab6b3a4132da0f21d43e8127bd8
Sean Rabaut <rabautse@gmail.com>
2014-09-06 02:36:20 -0400

    initial

---
+++ app/controllers/readings_controller.rb
@@ -0,0 +1,10 @@
+class ReadingsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :managers, only: :create do
-    resources :drivers, except: [:new, :edit, :update]
+    resources :drivers, except: [:new, :edit, :update] do
+      resources :readings, except: [:new, :edit, :update, :destroy]
+    end
   end
 end
